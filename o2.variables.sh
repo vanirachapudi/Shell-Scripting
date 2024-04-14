9ba206c05057ce71d052c6074f6d91bad73ed9de
vani0432 <grandhivani@gmail.com>
2024-04-14 17:14:38 +0530

    shell

--- o2.variables.sh
+++ o2.variables.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-echo "ramesh:: hello suresh, good mrng"
-echo "suresh:: gi ramesh, very good mrng"
-echo "ramesh:: how are you"
-echo "suresh:: i am good how are you"
+PERSION1=ramesh
+PERSION2=suresh
+
+echo "$PERSION1:: hello $PERSION2, good mrng"
+echo "$PERSION2:: gi $PERSION1, very good mrng"
+echo "$PERSION1:: how are you"
+echo "$PERSION2:: i am good how are you"

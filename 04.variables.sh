acc290eb068abafa6111134a27c70694f7e83149
vani0432 <grandhivani@gmail.com>
2024-04-14 17:30:04 +0530

    shell

---
+++ 04.variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSION1=$1
+PERSION2=$2
+
+echo "$PERSION1:: hello $PERSION2, good mrng"
+echo "$PERSION2:: gi $PERSION1, very good mrng"
+echo "$PERSION1:: how are you"
+echo "$PERSION2:: i am good how are you"

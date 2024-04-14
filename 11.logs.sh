17db60cb88b26738eb83fab8b8b22845c4b4eb20
vani0432 <grandhivani@gmail.com>
2024-04-14 20:57:24 +0530

    shell

---
+++ 11.logs.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+ID=$(id -u)
+
+TIMESTAMP=$(date +%F-%H-%M-%S)
+
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo "ERROR: $2 ...... FAILED"
+        exit 1
+    else
+        echo "$2 ... SUCCESS"
+    fi
+
+}
+
+if [ $ID -ne 0 ]
+then
+    echo "ERROR: your are not root user"
+    exit 1
+else
+    echo "your root user"
+fi
+
+yum install mysql -y  &>> $LOGFILE
+
+VALIDATE $? "installing mysql"
+
+yum install git -y  &>> $LOGFILE
+
+VALIDATE  $? "installing git"

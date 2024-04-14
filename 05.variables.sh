4d9cc2a9baf6f93d57653ceacb3c345444cf389e
vani0432 <grandhivani@gmail.com>
2024-04-14 17:46:42 +0530

    shell

--- 05.variables.sh
+++ 05.variables.sh
@@ -4,4 +4,8 @@ echo "please enter username"
 
 read -s USERNAME
 
-echo "username is :$USERNAME"
+echo "please enter password"
+
+read -s PASSWORD
+
+echo "username is :$USERNAME, password is : $PASSWORD"
